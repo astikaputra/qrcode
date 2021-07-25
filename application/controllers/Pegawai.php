5efd363eab427368fc918df6ddb9e9ccf45325c4
I Wayan Astika Putra <astikaputra@gmail.com>
2021-07-25 22:53:42 +0800

    generate link barcode by current url

--- application/controllers/Pegawai.php
+++ application/controllers/Pegawai.php
@@ -85,6 +85,34 @@ class Pegawai extends CI_Controller{
         $queryPegawaiDetail = $this->pegawai->getPegawaiDetail($pegawai_id);
         $data =  array('queryPgwDetail' => $queryPegawaiDetail);
 
+        //Mengambil data url untuk di jadikan barcode
+        $dataBarcode = current_url();
+
+        // echo "<pre>";
+        // print_r($dataBarcode);
+        // echo"</pre>";
+
+
+        $this->load->library('ciqrcode'); //pemanggilan library QR CODE
+ 
+        $config['cacheable']    = true; //boolean, the default is true
+        $config['cachedir']     = './assets/'; //string, the default is application/cache/
+        $config['errorlog']     = './assets/'; //string, the default is application/logs/
+        $config['imagedir']     = './assets/images/'; //direktori penyimpanan qr code
+        $config['quality']      = true; //boolean, the default is true
+        $config['size']         = '1024'; //interger, the default is 1024
+        $config['black']        = array(224,255,255); // array, default is array(255,255,255)
+        $config['white']        = array(70,130,180); // array, default is array(0,0,0)
+        $this->ciqrcode->initialize($config);
+ 
+        $image_name=$pegawai_id.'.png'; //buat name dari qr code sesuai dengan nip
+ 
+        $params['data'] = $dataBarcode; //data yang akan di jadikan QR CODE
+        $params['level'] = 'H'; //H=High
+        $params['size'] = 10;
+        $params['savename'] = FCPATH.$config['imagedir'].$image_name; //simpan image QR CODE ke folder assets/images/
+        $this->ciqrcode->generate($params); // fungsi untuk generate QR CODE
+
         $this->load->view('v_detail_pegawai_pdf',$data);
 
 
